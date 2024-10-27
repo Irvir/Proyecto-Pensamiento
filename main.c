96d6308abfc308e2b9e49b207cd3fe14b25928b8
Martin Droguett <martin.dtondro@gmail.com>
2024-10-27 15:53:24 -0300

    Avances: mejor menú, encontrar cuenta de forma exitosa, creacion de los archivos, exitosa, solo falta agregar para las demas sucursales

--- main.c
+++ main.c
@@ -3,54 +3,95 @@
 #include <string.h>
 //hALO
 struct sucursales {
-    char *nombre;
-    char *apellidos;
+    char nombre[50];
+    char apellidos[50];
     int rut;
     int saldo;
     int interes;
+
 };
 struct cuenta_Corriente {
     int saldo;
     int interes;
+    int comision_mensual;
     int estado;
 };
 struct cuenta_Vista {
     int saldo;
     int interes;
+    int comision_mensual;
     int estado;
 };
 struct cuenta_Ahorro {
     int saldo;
     int interes;
+    int comision_mensual;
     int estado;
 };
-void encontrar_cuenta(struct sucursales s1,struct sucursales s2,struct sucursales s3,
-    struct sucursales s4,struct sucursales s5, int opcion,char *nombre,char *apellido,
-    int rut,int *resultado_final){
+
+void encontrar_cuenta(struct sucursales s1[],struct sucursales s2[],struct sucursales s3[],
+    struct sucursales s4[],struct sucursales s5[], int opcion,char *nombre,char *apellidos,
+    int rut,int *resultado_final,int iterador){
+
     FILE *archivo;
+    char nombre_archivo[50];
+
     switch(opcion) {
         case 1:
-            archivo = fopen("Sucursal1.txt","r");
-            if(archivo == NULL) {
-                archivo = fopen("Sucursal 1.txt","a+");
-                while(fscanf(archivo,"%50s %50 %i",nombre,apellido,rut)==3) {
-                }
-                *resultado_final=1;
-                fclose(archivo);
-            }
-            else {
-                  while(fscanf(archivo,"%50s %50 %i",nombre,apellido,rut)==3) {
-                  }
-            *resultado_final=1;
+            strcpy(nombre_archivo,"Sucursal 1.txt");
+            break;
+        case 2:
+            strcpy(nombre_archivo,"Sucursal 2.txt");
+            break;
+        case 3:
+            strcpy(nombre_archivo,"Sucursal 3.txt");
+            break;
+        case 4:
+            strcpy(nombre_archivo,"Sucursal 4.txt");
+            break;
+        case 5:
+            strcpy(nombre_archivo,"Sucursal 5.txt");
+            break;
+        default:
+            return;
+    }
 
-            }
+    char encontrar_nombre[50];
+    char encontrar_apellidos[50];
+    int encontrar_rut;
+    *resultado_final=1;
+    archivo = fopen(nombre_archivo,"r");
+    if(archivo == NULL) {
+        *resultado_final=1;
+        fclose(archivo);
+    }
+    else {
+        while(iterador<100 && s1[iterador].rut!=rut) {
+            iterador++;
+        }
+        strcpy(s1[iterador].nombre,*nombre);
+        strcpy(s1[iterador].rut,rut);
+        strcpy(s1[iterador].apellidos,*apellidos);
+        fclose(archivo);
+
+    }
+
+}
+void registrar_cuenta_sucursal(struct sucursales s1[], struct sucursales s2[], struct sucursales s3[], struct sucursales s4[],
+    struct sucursales s5[], int sucursal,char *nombre,char *apellidos,int rut) {
+    FILE *archivo;
+    switch(sucursal) {
+        case 1:
+            archivo = fopen("Sucursal 1.txt","a+");
+            fprintf(archivo,"Nombre: %s Apellidos: %s RUT: %i\n",nombre,apellidos,rut);
+            fclose(archivo);
             break;
         case 2:break;
         case 3:break;
         case 4:break;
         case 5:break;
+        default:return;
     }
-
 }
 //Apellidos,Nombre,Rut
 void menu(int opcion) {
@@ -60,30 +101,36 @@ void menu(int opcion) {
     getchar();
 
 
-    char *nombre=(char*)malloc(10*sizeof(char)*5);
-    char *apellidos=(char*)malloc(10*sizeof(char)*5);
+    char nombre[50];
+    char apellidos[50];
     int rut;
 
     int sucursal;
-    struct sucursales s1,s2,s3,s4,s5;
+    struct sucursales s1[100]={{0}};
+    struct sucursales s2[100]={{0}};
+    struct sucursales s3[100]={{0}};
+    struct sucursales s4[100]={{0}};
+    struct sucursales s5[100]={{0}};
+
     int estado_cuenta;
     int *resultado_final=(int*)malloc(10*sizeof(int));
+    int iterador=0;
 
     int decision_crear_cuenta;
 
     switch (opcion) {
         case 1:
-            printf("Ingrese su sucursal:\nSucursal 1.\nSucursal 2.\nSucursal 3.\nSucursal 4.\nSucursal 5.\n");
+            printf("Ingrese su sucursal:(1-5):");
             scanf("%i",&sucursal);
             switch (sucursal) {
                 case 1:
                     getchar();
-                    printf("Escriba su nombre:\n");
+                    printf("Escriba su nombre:");
                     fgets(nombre,50,stdin);
                     nombre[strcspn(nombre,"\n")]='\0';
 
-                    printf("%s\n",nombre);
-                    printf("Escriba sus apellidos:\n");
+
+                    printf("\nEscriba sus apellidos:");
                     fgets(apellidos,50,stdin);
                     apellidos[strcspn(apellidos,"\n")]='\0';
 
@@ -91,17 +138,21 @@ void menu(int opcion) {
                     scanf("%i",&rut);
                     getchar();
 
-                    encontrar_cuenta(s1,s2,s3,s4,s5,sucursal,nombre,apellidos,rut,resultado_final);
+                    encontrar_cuenta(s1,s2,s3,s4,s5,sucursal,nombre,apellidos,rut,resultado_final,iterador);
                     if(*resultado_final==1) {
                         printf("Estimado(a): %s %s no se encuentra registrado(a) en la sucursal %i.\nDesea crear una cuenta?\n 1.Si\n2.no",nombre,apellidos,opcion);
                         scanf("%i",&decision_crear_cuenta);
                         switch(decision_crear_cuenta) {
                             case 1:
+                                registrar_cuenta_sucursal(s1,s2,s3,s4,s5,sucursal,nombre,apellidos,rut);
                                 break;
                             case 2:
                                 opcion=0;
                                 break;
                         }
+                    }
+                    if(*resultado_final==0) {
+                        printf("Bienvenido estimado(a): %s %s rut:%i\n",nombre,apellidos,rut);
                     }
 
 
