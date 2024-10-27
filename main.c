b336314451021d48fa90e6320a2a2c5c75f57f65
Martin Droguett <martin.dtondro@gmail.com>
2024-10-27 20:11:39 -0300

    SIMPLIFICACION DE TODO PRIMERO

--- main.c
+++ main.c
@@ -29,35 +29,71 @@ struct cuenta_Ahorro {
     int estado;
 };
 
-void encontrar_cuenta(struct sucursales s1[],struct sucursales s2[],struct sucursales s3[],
-    struct sucursales s4[],struct sucursales s5[], int opcion,char *nombre,char *apellidos,
-    int rut,int *resultado_final,int iterador){
+void encontrar_cuenta(struct sucursales s1[],struct sucursales s2[],struct sucursales s3[],struct sucursales s4[],struct sucursales s5[],
+    int opcion,char *nombre,char *apellidos,int rut,int *resultado_final){
 
-    iterador = 0;
+    int iterador = 0;
     int bandera = 0;
-    *resultado_final = 1; // Inicialmente asumimos que no se encuentra la cuenta
-
-    while (iterador < 100 && bandera == 0) {
-        if (s1[iterador].rut == rut && strcmp(s1[iterador].apellidos,apellidos) == 0 && strcmp(s1[iterador].nombre,nombre) == 0) {
-            *resultado_final = 0; // Cuenta encontrada
-            bandera = 1;
+    *resultado_final = 1;
+    switch(opcion) {
+        case 1:
+            while (iterador < 100 && bandera == 0) {
+                if (s1[iterador].rut == rut && strcmp(s1[iterador].apellidos,apellidos) == 0 && strcmp(s1[iterador].nombre,nombre) == 0) {
+                    *resultado_final = 0;
+                    bandera = 1;
+                }
+                iterador++;
+            }
             break;
-        }
-        iterador++;
+
+        case 2:
+            while (iterador < 100 && bandera == 0) {
+                if (s2[iterador].rut == rut && strcmp(s2[iterador].apellidos,apellidos) == 0 && strcmp(s2[iterador].nombre,nombre) == 0) {
+                    *resultado_final = 0;
+                    bandera = 1;
+                }
+                iterador++;
+            }
+            break;
+        case 3:
+            while (iterador < 100 && bandera == 0) {
+                if (s3[iterador].rut == rut && strcmp(s3[iterador].apellidos,apellidos) == 0 && strcmp(s3[iterador].nombre,nombre) == 0) {
+                    *resultado_final = 0;
+                    bandera = 1;
+                }
+                iterador++;
+            }
+            break;
+        case 4:
+            while (iterador < 100 && bandera == 0) {
+                if (s4[iterador].rut == rut && strcmp(s4[iterador].apellidos,apellidos) == 0 && strcmp(s4[iterador].nombre,nombre) == 0) {
+                    *resultado_final = 0;
+                    bandera = 1;
+                }
+                iterador++;
+            }
+            break;
+        case 5:
+            while (iterador < 100 && bandera == 0) {
+                if (s5[iterador].rut == rut && strcmp(s5[iterador].apellidos,apellidos) == 0 && strcmp(s5[iterador].nombre,nombre) == 0) {
+                    *resultado_final = 0;
+                    bandera = 1;
+                }
+                iterador++;
+            }
+            break;
     }
+
 }
 
 
 void registrar_cuenta_sucursal(struct sucursales s1[], struct sucursales s2[], struct sucursales s3[], struct sucursales s4[],
     struct sucursales s5[], int sucursal,char *nombre,char *apellidos,int rut) {
-
-
-    FILE *archivo;
     int iterador=0;
     int bandera=0;
+    //Estructura
     switch(sucursal) {
         case 1:
-            //Estructura
             while(iterador<100  && bandera==0) {
                 if(s1[iterador].rut==0) {
                     strcpy(s1[iterador].nombre,nombre);
@@ -67,24 +103,82 @@ void registrar_cuenta_sucursal(struct sucursales s1[], struct sucursales s2[], s
                 }
                 iterador++;
             }
-            //Archivo
-            archivo = fopen("Sucursal 1.txt","a+");
-            fprintf(archivo,"Nombre: %s Apellidos: %s RUT: %i\n",nombre,apellidos,rut);
-            fclose(archivo);
+            break;
+        case 2:
+            while(iterador<100  && bandera==0) {
+                if(s2[iterador].rut==0) {
+                    strcpy(s2[iterador].nombre,nombre);
+                    strcpy(s2[iterador].apellidos,apellidos);
+                    s2[iterador].rut=rut;
+                    bandera=1;
+                }
+                iterador++;
+            }
+            break;
+        case 3:
+            while(iterador<100  && bandera==0) {
+                if(s3[iterador].rut==0) {
+                    strcpy(s3[iterador].nombre,nombre);
+                    strcpy(s3[iterador].apellidos,apellidos);
+                    s3[iterador].rut=rut;
+                    bandera=1;
+                }
+                iterador++;
+            }
+            break;
+        case 4:
+            while(iterador<100  && bandera==0) {
+                if(s4[iterador].rut==0) {
+                    strcpy(s4[iterador].nombre,nombre);
+                    strcpy(s4[iterador].apellidos,apellidos);
+                    s4[iterador].rut=rut;
+                    bandera=1;
+                }
+                iterador++;
+            }
+            break;
+        case 5:
+            while(iterador<100  && bandera==0) {
+                if(s5[iterador].rut==0) {
+                    strcpy(s5[iterador].nombre,nombre);
+                    strcpy(s5[iterador].apellidos,apellidos);
+                    s5[iterador].rut=rut;
+                    bandera=1;
+                }
+                iterador++;
+            }
+            break;
+    }
+
+
+}
+void imprimir(struct sucursales s1[],struct sucursales s2[],struct sucursales s3[],struct sucursales s4[],struct sucursales s5[], int cantidad,int opcion) {
+
+    switch (opcion) {
+        case 1:
+            printf("Lista de sucursales:\n");
+            for (int i = 0; i < cantidad; i++) {
+                if (s1[i].rut != 0) {
+                    printf("Nombre: %s\n", s1[i].nombre);
+                    printf("Apellidos: %s\n", s1[i].apellidos);
+                    printf("Rut: %i\n", s1[i].rut);
+                    printf("---------------------\n");
+                    }
+                }
             break;
         case 2:break;
         case 3:break;
         case 4:break;
         case 5:break;
-        default:return;
     }
+
 }
 //Apellidos,Nombre,Rut
-void menu(int opcion) {
+void menu(int opcion,struct sucursales s1[],struct sucursales s2[],struct sucursales s3[],struct sucursales s4[],struct sucursales s5[]) {
     FILE *archivo;
     printf("Menu de opciones:\n1.Ver Cuenta\n2.Crear Cuenta.\n3.Operacion de cuentas.\n0.Salir\nEscriba su opcion:");
     scanf("%i",&opcion);
-    getchar();
+
 
 
     char nombre[50];
@@ -92,15 +186,10 @@ void menu(int opcion) {
     int rut;
 
     int sucursal;
-    struct sucursales s1[100]={{0}};
-    struct sucursales s2[100]={{0}};
-    struct sucursales s3[100]={{0}};
-    struct sucursales s4[100]={{0}};
-    struct sucursales s5[100]={{0}};
+
 
     int estado_cuenta;
-    int *resultado_final=(int*)malloc(10*sizeof(int));
-    int iterador=0;
+    int *resultado_final=(int*)malloc(sizeof(int)*1);
 
     int decision_crear_cuenta;
 
@@ -124,13 +213,15 @@ void menu(int opcion) {
                     scanf("%i",&rut);
                     getchar();
 
-                    encontrar_cuenta(s1,s2,s3,s4,s5,sucursal,nombre,apellidos,rut,resultado_final,iterador);
+                    encontrar_cuenta(s1,s2,s3,s4,s5,sucursal,nombre,apellidos,rut,resultado_final);
                     if(*resultado_final==1) {
                         printf("Estimado(a): %s %s no se encuentra registrado(a) en la sucursal %i.\nDesea crear una cuenta?\n 1.Si\n2.no",nombre,apellidos,opcion);
                         scanf("%i",&decision_crear_cuenta);
+                        getchar();
                         switch(decision_crear_cuenta) {
                             case 1:
                                 registrar_cuenta_sucursal(s1,s2,s3,s4,s5,sucursal,nombre,apellidos,rut);
+                                imprimir(s1,s2,s3,s4,s5,100,opcion);
                                 break;
                             case 2:
                                 opcion=0;
@@ -147,15 +238,22 @@ void menu(int opcion) {
             break;
         case 2:
             printf("Ingrese su sucursal");
+            break;
         case 3:
             printf("Ingrese su sucursal");
+            break;
         case 0:
             break;
     }
 
 }
 int main(void) {
-    menu(0);
 
+    struct sucursales s1[100]={0};
+    struct sucursales s2[100]={0};
+    struct sucursales s3[100]={0};
+    struct sucursales s4[100]={0};
+    struct sucursales s5[100]={0};
+    menu(0,s1,s2,s3,s4,s5);
     return 0;
 }
